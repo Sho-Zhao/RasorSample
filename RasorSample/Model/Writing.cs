4d9d76afaff7a65cdd5ed31aba5f9ede0982af37
Sho-Zhao <showtalo@nifty.com>
2021-02-15 19:15:39 +0900

    プロジェクト ファイルを追加します。

---
+++ RasorSample/Model/Writing.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace RasorSample.Model
+{
+    public class Writing
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Comment { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+    }
+}
